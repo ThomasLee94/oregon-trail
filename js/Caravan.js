0051e591f281018c305d71518c737b5dea984c29
Thomas <thomaslee9474@gmail.com>
2019-02-12 14:01:05 -0800

    [added] event class

--- js/Caravan.js
+++ js/Caravan.js
@@ -6,8 +6,8 @@ class Caravan {
   }) {
     this.WEIGHT_PER_OX = 20;
     this.WEIGHT_PER_PERSON = 2;
-    this.FOOD_WEIGHT = 0.6;
-    this.FIREPOWER_WEIGHT = 5;
+    this.WEIGHT_OF_FOOD = 0.6;
+    this.WEIGHT_OF_FIREPOWER = 5;
     this.GAME_SPEED = 800;
     this.DAY_PER_STEP = 0.2;
     this.FOOD_PER_PERSON = 0.02;
@@ -29,18 +29,15 @@ class Caravan {
   updateWeight() {
     let droppedFood = 0;
     let droppedGuns = 0;
-
     // how much can the caravan carry
     this.capacity = this.oxen * this.WEIGHT_PER_OX + this.crew * this.WEIGHT_PER_PERSON;
-
     // how much weight do we currently have
-    this.weight = this.food * this.FOOD_WEIGHT + this.firepower * this.FIREPOWER_WEIGHT;
-
+    this.weight = this.food * this.WEIGHT_OF_FOOD + this.firepower * this.WEIGHT_OF_FIREPOWERT;
     // drop things behind if it's too much weight
     // assume guns get dropped before food
     while (this.firepower && this.capacity <= this.weight) {
       this.firepower -= 1;
-      this.weight -= this.FIREPOWER_WEIGHT;
+      this.weight -= this.WEIGHT_OF_FIREPOWERT;
       droppedGuns += 1;
     }
 
@@ -50,7 +47,7 @@ class Caravan {
 
     while (this.food && this.capacity <= this.weight) {
       this.food -= 1;
-      this.weight -= this.FOOD_WEIGHT;
+      this.weight -= this.WEIGHT_OF_FOOD;
       droppedFood += 1;
     }
 
@@ -64,8 +61,8 @@ class Caravan {
     const diff = this.capacity - this.weight;
     const speed = this.SLOW_SPEED + diff / this.capacity * this.FULL_SPEED;
     this.distance += speed;
-  };
-  
+  }
+
   // food consumption
   consumeFood() {
     this.food -= this.crew * this.FOOD_PER_PERSON;
@@ -73,8 +70,8 @@ class Caravan {
     if (this.food < 0) {
       this.food = 0;
     }
-  };
+  }
 }
 
-// init game
+// INIT CARAVAN
 const caravan = new Caravan({day: 0,distance: 0, crew: 30, food: 80, oxen: 2, money: 300,firepower: 2,})
